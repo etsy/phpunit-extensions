de0eaf24a6f7767fc0f170a962b90f01816de9b4
llincoln <llincoln@etsy.com>
2011-12-21 21:14:25 +0000

    Add a the capability to set a default return value in the return value mapping builder.

--- PHPUnit/Extensions/MockObject/Stub/ReturnMapping.php
+++ PHPUnit/Extensions/MockObject/Stub/ReturnMapping.php
@@ -4,13 +4,21 @@ class PHPUnit_Extensions_MockObject_Stub_ReturnMapping
 implements PHPUnit_Framework_MockObject_Stub {
 
     protected $return_map;
+    protected $default;
 
     /**
      * @param array $return_map an array of 
      * PHPUnit_Extensions_Mock_Object_Stub_ReturnMapping_Entry.
+     * @param PHPUnit_Extensions_MockObject_Stub_ReturnMapping_Entry $default
+     * the default value to return in the event that no other parameters
+     * matched.
      */
-    public function __construct(array $return_map) {
+    public function __construct(
+        array $return_map,
+        $default = NULL
+    ) {
         $this->return_map = $return_map;
+        $this->default = $default;
     }
 
     /**
@@ -23,6 +31,11 @@ implements PHPUnit_Framework_MockObject_Stub {
                 return $entry->invoke($invocation);
             }
         }
+
+        if ($this->default != NULL) {
+            return $this->default->invoke($invocation);
+        }
+
         PHPUnit_Framework_Assert::fail(
             sprintf(
                 'No return value defined for %s', 
--- PHPUnit/Extensions/MockObject/Stub/ReturnMapping/Builder.php
+++ PHPUnit/Extensions/MockObject/Stub/ReturnMapping/Builder.php
@@ -3,9 +3,11 @@
 class PHPUnit_Extensions_MockObject_Stub_ReturnMapping_Builder {
 
     private $entry_builders;
+    private $default_entry;
 
     public function __construct() {
         $this->entry_builders = array();
+        $this->default_entry = NULL;
     }
 
     /**
@@ -19,6 +21,25 @@ class PHPUnit_Extensions_MockObject_Stub_ReturnMapping_Builder {
     }
 
     /**
+     * Sets the default return value.
+     *
+     * @param PHPUnit_Extensions_MockObject_Stub|mixed $default the default
+     * return value.
+     * @return PHPUnit_Extensions_MockObject_Stub_ReturnMapping_Builder
+     */
+    public function setDefault($default) {
+        $entry_builder =
+            new PHPUnit_Extensions_MockObject_Stub_ReturnMapping_EntryBuilder();
+        $entry_builder
+            ->with($this->anything())
+            ->will($default);
+
+        $this->default_entry = $entry_builder->build();
+
+        return $this;
+    }
+
+    /**
      * @return PHPUnit_Extensions_MockObject_Stub_ReturnMapping
      */
     public function build() {
@@ -26,7 +47,10 @@ class PHPUnit_Extensions_MockObject_Stub_ReturnMapping_Builder {
         foreach ($this->entry_builders as $builder) {
             $entries[] = $builder->build();
         }
-        return new PHPUnit_Extensions_MockObject_Stub_ReturnMapping($entries);
+        return new PHPUnit_Extensions_MockObject_Stub_ReturnMapping(
+            $entries,
+            $this->default_entry
+        );
     }
 }
 
