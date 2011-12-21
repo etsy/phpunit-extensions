e2837ff9b32972e8252f48db1219bb4fe4fe3ab5
llincoln <llincoln@etsy.com>
2011-12-21 20:54:59 +0000

    Removed SameSize as it is in PHPUnit 3.6
    Remove Jira from the package.xml as we removed Jira in a previous commit.
    
    Update all of the constraints to match the interface changes introduced in PHPUnit 3.6 and update all the tests.
    
    Increment the package.xml version number.
    
    Next change will add a feature to the return map builder.

--- PHPUnit/Extensions/Constraint/ArrayEqualsNoOrder.php
+++ PHPUnit/Extensions/Constraint/ArrayEqualsNoOrder.php
@@ -5,26 +5,17 @@
  * but not necessarily the same order.
  */
 class PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder
-extends PHPUnit_Framework_Constraint {
+extends PHPUnit_Framework_Constraint_And {
 
     private $andConstraint;
 
     public function __construct($expected) {
-        $this->andConstraint = new PHPUnit_Framework_Constraint_And();
-        $this->andConstraint->setConstraints(
+        $this->setConstraints(
             array(
                 new PHPUnit_Extensions_Constraint_HasItems($expected),
-                new PHPUnit_Extensions_Constraint_SameSize($expected)
+                new PHPUnit_Framework_Constraint_SameSize($expected)
             )
         );
-    }
-
-    protected function matches($other) {
-        return $this->andConstraint->matches($other);
-    }
-
-    public function toString() {
-        return $this->andConstraint->toString();
     }
 }
 
--- Tests/Extensions/Constraint/ArrayEqualsNoOrderTest.php
+++ Tests/Extensions/Constraint/ArrayEqualsNoOrderTest.php
@@ -1,6 +1,9 @@
 <?php
 
+require_once 'PHPUnit/Extensions/Assert/More.php';
+require_once 'PHPUnit/Extensions/Constraint/HasItems.php';
 require_once 'PHPUnit/Extensions/Constraint/ArrayEqualsNoOrder.php';
+require_once 'PHPUnit/Extensions/Constraint/StringMatchIgnoreWhitespace.php';
 
 /**
  * @covers PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder
@@ -8,86 +11,113 @@ require_once 'PHPUnit/Extensions/Constraint/ArrayEqualsNoOrder.php';
 class Tests_Extensions_Constraint_ArrayEqualsNoOrderTest
 extends PHPUnit_Framework_TestCase {
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_null() {
         $constraint =
             new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(null);
-        $this->assertFalse($constraint->evaluate(null));
+        $constraint->evaluate(null);
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_nullExpected() {
         $constraint =
             new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(null);
-        $this->assertFalse($constraint->evaluate(array(1, 2, 3)));
+        $constraint->evaluate(array(1, 2, 3));
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_nullActual() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array(1, 2, 3)
         );
-        $this->assertFalse($constraint->evaluate(null));
+        $constraint->evaluate(null);
     }
 
     public function testEvaluate_empty() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array()
         );
-        $this->assertTrue($constraint->evaluate(array()));
+        $constraint->evaluate(array());
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_emptyExpected() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array()
         );
-        $this->assertFalse($constraint->evaluate(array(1, 2, 3)));
+        $constraint->evaluate(array(1, 2, 3));
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_emptyActual() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array(1, 2, 3)
         );
-        $this->assertFalse($constraint->evaluate(array()));
+        $constraint->evaluate(array());
     }
 
     public function testEvaluate_equal() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array(1, 2, 3)
         );
-        $this->assertTrue($constraint->evaluate(array(1, 2, 3)));
+        $constraint->evaluate(array(1, 2, 3));
     }
 
     public function testEvaluate_sameDifferentOrder() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array(1, 2, 3)
         );
-        $this->assertTrue($constraint->evaluate(array(3, 1, 2)));
+        $constraint->evaluate(array(3, 1, 2));
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_duplicateExpected() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array(1, 1, 2, 2, 3, 3)
         );
-        $this->assertFalse($constraint->evaluate(array(1, 2, 3)));
+        $constraint->evaluate(array(1, 2, 3));
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_duplicateActual() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array(1, 2, 3)
         );
-        $this->assertFalse($constraint->evaluate(array(1, 1, 2, 2, 3, 3)));
+        $constraint->evaluate(array(1, 1, 2, 2, 3, 3));
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_moreThanExpected() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array(1, 2, 3)
         );
-        $this->assertFalse($constraint->evaluate(array(1, 0, 2, 3)));
+        $constraint->evaluate(array(1, 0, 2, 3));
     }
 
+    /**
+     * @expectedException PHPUnit_Framework_AssertionFailedError
+     */
     public function testEvaluate_lessThanExpected() {
         $constraint = new PHPUnit_Extensions_Constraint_ArrayEqualsNoOrder(
             array(1, 0, 2, 3)
         );
-        $this->assertFalse($constraint->evaluate(array(1, 2, 3)));
+        $constraint->evaluate(array(1, 2, 3));
     }
 
     public function testToString() {
@@ -95,7 +125,7 @@ extends PHPUnit_Framework_TestCase {
             array(1, 2, 3)
         );
         PHPUnit_Extensions_Assert_More::assertStringMatchIgnoreWhitespace(
-            'has items Array ( [0] => 1 [1] => 2 [2] => 3 ) and is same size as Array ( [0] => 1 [1] => 2 [2] => 3 )',
+            'has items Array ( 0 => 1 1 => 2 2 => 3 ) and count matches',
             $constraint->toString());
     }
 }
